e47e737bdc3be673b92626157606cff7363c9214
Nathã Wolff <natha.wolff9@gmail.com>
2023-03-29 01:54:26 -0300

    estrutura pronta

--- react_sass/src/components/MainContent.jsx
+++ react_sass/src/components/MainContent.jsx
@@ -1,10 +1,17 @@
-import "../styles/components/maincontent.sass";
+import TechnologiesContainer from "./TechnologiesContainer";
+import AboutContainer from "./AboutContainer";
+import ProjectsContainer from "./ProjectsContainer";
 
+import "../styles/components/maincontent.sass";
 
 const MainContent = () => {
   return (
-    <main id = "main-content">MainContent</main>
-  )
-}
+    <main id="main-content">
+      <AboutContainer />
+      <TechnologiesContainer />
+      <ProjectsContainer />
+    </main>
+  );
+};
 
-export default MainContent
+export default MainContent;
